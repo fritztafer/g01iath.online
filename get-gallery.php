88c48a03b8b1a6754b6de53902f8e77728e3ba89
fritztafer <fritzistheman@gmail.com>
2025-04-25 17:56:20 -0700

    hid file-list.json to sort to top

--- get-gallery.php
+++ get-gallery.php
@@ -4,5 +4,5 @@ $images = array_values(array_filter(scandir($dir), function($file) use ($dir) {
     return preg_match('/\.(jpe?g|png|gif|webp)$/i', $file) && is_file($dir . $file);
 }));
 
-file_put_contents($dir . 'file-list.json', json_encode($images, JSON_PRETTY_PRINT));
+file_put_contents($dir . '.file-list.json', json_encode($images, JSON_PRETTY_PRINT));
 echo "generated file-list.json with " . count($images) . " files";
