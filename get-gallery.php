2120df0c6496742fc4f2d9f7aa0ddecaa405ccd6
fritztafer <fritzistheman@gmail.com>
2025-05-02 15:30:52 -0700

    Added support for .txt files in aesthetic

--- get-gallery.php
+++ get-gallery.php
@@ -1,7 +1,7 @@
 <?php
 $dir = 'public/gallery/';
 $images = array_values(array_filter(scandir($dir), function($file) use ($dir) {
-    return preg_match('/\.(jpe?g|png|gif|webp)$/i', $file) && is_file($dir . $file);
+    return preg_match('/\.(jpe?g|png|gif|webp|txt)$/i', $file) && is_file($dir . $file);
 }));
 
 file_put_contents($dir . '.file-list.json', json_encode($images, JSON_PRETTY_PRINT));
--- public/js/aesthetic.js
+++ public/js/aesthetic.js
@@ -2,17 +2,13 @@
 
 var index = 0, // track which files loaded
     amount = 16, // amount of files per load
-    images = [], // hold img file names
-    timeout, // debounce scrollHandler()
-    loading = false, // debounce loadImages()
+    files = [], // holds file names
+    timeout, // debounce scroll handler
+    loading = false, // debounce loadItems()
     ready = fetch("gallery/.file-list.json")
-    .then(res => res.json())
-    .then(array => {
-        images = shuffleArray(array);
-    })
-    .catch(err => {
-        console.error("error loading gallery", err);
-    });
+        .then(res => res.json())
+        .then(array => {files = shuffleArray(array);})
+        .catch(err => {console.error("error loading gallery", err);});
 
 async function aesthetic() {
     await ready;
@@ -25,7 +21,7 @@ async function aesthetic() {
             else if (window.innerWidth >= 1280) {return 4;}
         })();
     loadColumns(colAmt, parent);
-    loadImages(parent);
+    loadItems(parent);
 
     main.appendChild(parent);
     setTimeout(() => {
@@ -36,38 +32,56 @@ async function aesthetic() {
     }, time);
 }
 
-function loadImages(parent) {
-    if (loading || index > images.length) return;
+function loadItems(parent) {
+    if (loading || index > files.length) return;
     loading = true;
     let columns = parent.childNodes;
-    images.slice(index, index + amount).map(filename => {
-        let img = Object.assign(document.createElement("img"),{
-            className: "aesthetic-item",
-            src: `gallery/${filename}`,
-            alt: "",
-            onload: () => {
-                let lengths = Array.from(columns).map(col => col.offsetHeight);
-                let column = lengths.indexOf(Math.min(...lengths));
-                columns[column].appendChild(img);
-                setTimeout(() => {img.classList.add('loaded');}, 100); // timeout for animation
-            }
-        });
+    files.slice(index, index + amount).map(filename => {
+        if (filename.lastIndexOf(".txt") === filename.length - 4) {
+            !async function(){
+                let item = Object.assign(document.createElement("div"),{className: "aesthetic-item"}),
+                    quote = Object.assign(document.createElement("div"),{className: "aesthetic-item-quote"}),
+                    person = Object.assign(document.createElement("div"),{className: "aesthetic-item-person"}),
+                    text = await fetch(`gallery/${filename}`)
+                        .then(response => response.text())
+                        .then((data) => {return data.split("\r\n");}) // txt file formatting!
+                        .catch(error => {console.error(error);});
+                quote.innerHTML = text[0];
+                person.innerHTML = text[1];
+                item.append(quote, person);
+                renderItem(item, columns);
+            }();
+        } else {
+            let img = Object.assign(document.createElement("img"),{
+                className: "aesthetic-item",
+                src: `gallery/${filename}`,
+                alt: "",
+                onload: () => {renderItem(img, columns);}
+            });
+        }
     });
     index += amount;
     loading = false;
 }
 
-function adoptImages(colAmt) {
+function renderItem(file, columns) {
+    let lengths = Array.from(columns).map(col => col.offsetHeight);
+    let column = lengths.indexOf(Math.min(...lengths));
+    columns[column].appendChild(file);
+    setTimeout(() => {file.classList.add('loaded');}, 100); // timeout for animation
+}
+
+function adoptItems(colAmt) {
     let parent = document.getElementsByClassName("aesthetic")[0];
     let columns = document.getElementsByClassName("aesthetic-column");
     let amount = Math.max(...Array.from(columns).map(col => col.childNodes.length));
     let foster = [];
-    let newScroll = (window.scrollY / document.body.scrollHeight) * document.body.scrollHeight;
+    let ratio = window.scrollY / document.body.scrollHeight;
 
-    // hide so images don't fly around
+    // hide so items don't fly around
     parent.style.visibility = "hidden";
 
-    // get currently loaded images in order and push them to foster
+    // get currently loaded files by display order and push them to foster
     for (let i = 0; i < amount; i++) {
         for (let column of columns) {
             if (column.childNodes[i]) {
@@ -87,8 +101,8 @@ function adoptImages(colAmt) {
         columns[column].appendChild(child);
     }
 
-    // scroll roughly to images that were displayed
-    window.scrollTo({top: newScroll, behavior: "auto"});
+    // scroll roughly to files that were displayed
+    window.scrollTo({top: ratio * document.body.scrollHeight, behavior: "auto"});
 
     // unhide
     parent.style.visibility = "visible";
@@ -109,20 +123,20 @@ function shuffleArray(array) { // Durstenfeld shuffle
     return array;
 }
 
- function resizeHandler() {
+function resizeHandler() {
     let columns = document.getElementsByClassName("aesthetic-column")
-         if (window.innerWidth < 640 && columns.length !== 1) {adoptImages(1);}
-    else if (window.innerWidth >= 640 && window.innerWidth < 960 && columns.length !== 2) {adoptImages(2);}
-    else if (window.innerWidth >= 960 && window.innerWidth < 1280 && columns.length !== 3) {adoptImages(3);}
-    else if (window.innerWidth >= 1280 && columns.length !== 4) {adoptImages(4);}
+         if (window.innerWidth < 640 && columns.length !== 1) {adoptItems(1);}
+    else if (window.innerWidth >= 640 && window.innerWidth < 960 && columns.length !== 2) {adoptItems(2);}
+    else if (window.innerWidth >= 960 && window.innerWidth < 1280 && columns.length !== 3) {adoptItems(3);}
+    else if (window.innerWidth >= 1280 && columns.length !== 4) {adoptItems(4);}
 };
 
 function scrollHandler() {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
         let bottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 200;
-        if (bottom && index < images.length && !loading) {loadImages(document.getElementsByClassName("aesthetic")[0]);}
-        else if (index >= images.length && !loading) {window.removeEventListener("scroll", scrollHandler);}
+        if (bottom && index < files.length && !loading) {loadItems(document.getElementsByClassName("aesthetic")[0]);}
+        else if (index >= files.length && !loading) {window.removeEventListener("scroll", scrollHandler);}
     }, 100);
 }
 
