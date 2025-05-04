e4eca4ead29569fc526dc1c56fe3f8c2c82d1c0f
fritztafer <fritzistheman@gmail.com>
2025-05-03 20:27:13 -0700

    Completed about for now. Ready to public host

--- public/js/about.js
+++ public/js/about.js
@@ -2,19 +2,21 @@
 
 function about() {
     let main = document.querySelector("main"),
-        parent = Object.assign(document.createElement("div"),{className: "about", style: "width: 900px; margin: auto;"}),
+        parent = Object.assign(document.createElement("div"),{className: "about", style: "max-width: 315px; margin: auto;"}),
+        aboutContact = Object.assign(document.createElement("div"),{
+            style: "text-align: center; width: 300px; margin: auto; font-size: 21px; margin-bottom: 24px;",
+            innerHTML: 'Contact: <a href="mailto:fritz@g01iath.online">fritz@g01iath.online</a>'
+        }),
         aboutImg = Object.assign(document.createElement("div"),{
-            style: "float: left; text-align: center; width: 300px; padding-right: 16px;",
-            innerHTML: '<img src="img/2012.jpg" style="height: auto; width: inherit; margin-bottom: -10px;"><div style="width: inherit;">circa 2012</div>'
+            style: "text-align: center; width: 300px; margin: auto;",
+            innerHTML: '<img src="img/2012.jpg" style="height: auto; width: inherit;">'
         }),
-        aboutBio = Object.assign(document.createElement("span"),{className: "about-bio", style: "width: 600px; margin: auto;", innerText: 
-            `Music has always made me feel deeply, espicially in my childhood. If you are reading this I wouldn't doubt if it means a lot to you. The dreamer lost in imagination; self-image becoming skewed. Get real. Why spend so much time on something that gives so little in return?
-
-            When no one is there for you that song can pick you up. Those lyrics speak to your soul, the chords and that melody make you cry. You play your favorite song and feel inspired to be the best version of yourself. I hope my music makes at least someone feel this way, then it'll all have been worth it.
-            
-            Figuratively speaking, music is the Goliath to my David. This journey has brought more pain than joy, I won't lie...but if you're reading this...I slayed the giant and the beast's head lay severed at my feet. If you are familiar with the story you know what happens next...`
+        aboutBio = Object.assign(document.createElement("div"),{className: "about-bio", style: "text-align: center; margin: auto;", innerHTML: 
+            `
+            <div style="font-size: 36px; letter-spacing: 24px; margin-right: -24px; line-height: 1; margin-bottom: 24px;">GØ1IATH</div>
+            `
         }),
-        children = [aboutImg, aboutBio];
+        children = [aboutContact, aboutImg, aboutBio];
 
     for (let child of children) {parent.append(child);}
     setTimeout(() => {
