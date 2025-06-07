bde373bda69c7a5d02f40d96c760fd3357971a1b
hanginout <fritzistheman@gmail.com>
2025-06-07 14:31:12 -0700

    Changed listen to 1 column
    Changed song sources in listen

--- public/js/listen.js
+++ public/js/listen.js
@@ -3,12 +3,7 @@
 function listen() {
     let parent = Object.assign(document.createElement("div"),{className: "listen"}),
         sources = [
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1354507834&show_artwork=true&show_comments=false",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1289073427&show_artwork=true&show_comments=false",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1242933133&show_artwork=true&show_comments=false",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1237324432&show_artwork=true&show_comments=false",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1215322066&show_artwork=true&show_comments=false",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F948315661&show_artwork=true&show_comments=false"
+            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F2109535224&show_artwork=true&show_comments=false&secret_token=s-64sQfsfzWkM"
         ];
 
     for (let src of sources) {
