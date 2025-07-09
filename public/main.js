ad780d3c45308d7fa2bb3a13852f06b009b00982
fritztafer <fritzistheman@gmail.com>
2025-07-09 16:38:33 -0700

    remove silly spaces

--- public/main.js
+++ public/main.js
@@ -4,14 +4,14 @@ document.body.append(
     Object.assign(document.createElement("main")),
     Object.assign(document.createElement("footer"))
 );
-document.querySelector("header").innerHTML = 
+document.querySelector("header").innerHTML =
     `<div class="title">GØ1IATH</div>
     <a class="header-item" href="javascript:" onclick="run('socials')">SOCIALS</a>
     <a class="header-item" href="javascript:" onclick="run('listen')">LISTEN</a>
     <a class="header-item" href="javascript:" onclick="run('aesthetic')">AESTHETIC</a>
     <a class="header-item" href="javascript:" onclick="run('about')">ABOUT</a>
     <hr>`;
-document.querySelector("footer").innerHTML = 
+document.querySelector("footer").innerHTML =
     `<div class="footer-item">//G01IATH.ONLINE/2025/</div>`;
 
 let time = 2501;
