6318594070f558166a237e9c955888e1c9bdb9d3
fritztafer <fritzistheman@gmail.com>
2025-08-07 22:38:57 -0700

    Added get-gallery.ps1 to remove php dependency

---
+++ get-gallery.ps1
@@ -0,0 +1,27 @@
+$TargetDir = "./public/gallery"
+$OutputFile = "$TargetDir/.file-list.json"
+
+$extensionsPattern = '\.(jpe?g|png|gif|webp|txt)$'
+
+$files = Get-ChildItem -Path $TargetDir -File |
+    Where-Object { $_.Name -match $extensionsPattern } |
+    Select-Object -ExpandProperty Name
+
+$lines = @()
+$lines += '['
+for ($i = 0; $i -lt $files.Count; $i++) {
+    $line = '    "' + $files[$i] + '"'
+    if ($i -lt $files.Count - 1) {
+        $line += ','
+    }
+    $lines += $line
+}
+$lines += ']'
+
+$lines | Set-Content -Encoding UTF8 $OutputFile
+
+if ($files.Count -gt 0) {
+    $files.Count | Write-Output
+} else {
+    0 | Write-Output
+}
