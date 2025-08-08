6f68777bb9cbd981948f849aca790276edfb090b
fritztafer <fritzistheman@gmail.com>
2025-08-08 13:30:01 -0700

    Added get-gallery.sh to remove php dependency
    Deleted get-gallery.php

--- get-gallery.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dir = 'public/gallery/';
-$fname = '.file-list.json';
-$files = array_values(array_filter(scandir($dir), function($file) use ($dir) {return preg_match('/\.(jpe?g|png|gif|webp|txt)$/i', $file) && is_file($dir . $file);}));
-file_put_contents($dir . $fname, json_encode($files, JSON_PRETTY_PRINT));
-echo "generated ./" . $dir . $fname . " using " . count($files) . " files";
