4aada21bb91708640309af5558a18de598550ebc
fritztafer <fritzistheman@gmail.com>
2025-08-07 20:59:06 -0700

    Added get-gallery.sh to remove php dependency

---
+++ get-gallery.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET_DIR="./public/gallery"
+OUTPUT_FILE="$TARGET_DIR/.file-list.json"
+
+mapfile -t files < <(ls "$TARGET_DIR" | grep -Ei "\.(jpe?g|png|gif|webp|txt)$")
+
+{
+    printf '[\n'
+    for i in "${!files[@]}"; do
+        printf '    "%s"' "${files[i]}"
+        [[ $i -lt $((${#files[@]} - 1)) ]] && printf ','
+        printf '\n'
+    done
+    printf ']\n'
+} > "$OUTPUT_FILE"
+
+head -n -1 "$OUTPUT_FILE" | tail -n +2 | wc -l
