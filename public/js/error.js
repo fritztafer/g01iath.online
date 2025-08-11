dd39fb31c214597408c05161b8e16d4cb226a509
fritztafer <fritzistheman@gmail.com>
2025-08-11 16:54:20 -0700

    Added error handling files

---
+++ public/js/error.js
@@ -0,0 +1,9 @@
+function error() {
+    const statusCode = window.performance.getEntriesByType('navigation')[0].responseStatus ?? "CHECK CONSOLE";
+
+    return Object.assign(document.createElement("div"),{
+        className: "error",
+        style: "text-align: center; font-size: 18px; visibility: hidden; max-height: 0; overflow: hidden;",
+        innerHTML: `HTTP ERROR - ${statusCode}<br><br><a href="/">HOME</a>`
+    });
+}
