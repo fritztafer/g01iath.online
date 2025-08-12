91eec5ebcb6c1064040cab3e5a5436a70eb558d5
fritztafer <fritzistheman@gmail.com>
2025-08-12 11:40:54 -0700

    Added contact info

--- public/js/error.js
+++ public/js/error.js
@@ -4,13 +4,6 @@ function error() {
     return Object.assign(document.createElement("div"),{
         className: "error",
         style: "text-align: center; font-size: 18px; visibility: hidden; max-height: 0; overflow: hidden;",
-        innerHTML:
-            `HTTP ERROR - ${statusCode}
-            <br>
-            <br>
-            <a href="/">HOME</a>
-            <br>
-            <br>
-            <a href="mailto:fritz@g01iath.online">fritz@g01iath.online</a>`
+        innerHTML: `HTTP ERROR - ${statusCode}<br><br><a href="/">HOME</a><br><br><a href="mailto:fritz@g01iath.online">fritz@g01iath.online</a>`
     });
 }
