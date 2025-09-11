bdbb035ae8f7bc9508084b85d54a41dbae9155ea
fritztafer <fritzistheman@gmail.com>
2025-09-11 15:18:35 -0700

    Swapped socials img to icons
    Swapped socials href to fritztafer pages
    
    Removed unneeded styling
    
    Styled socials for consistent display
    
    Simplified socials.js

--- public/js/socials.js
+++ public/js/socials.js
@@ -1,10 +1,12 @@
 async function socials() {
     let parent = Object.assign(document.createElement("div"),{className: "socials", style: "visibility: hidden; max-height: 0; overflow: hidden;"}),
-        socialItem1 = Object.assign(document.createElement("a"),{className: "socials-item one", target: "_blank", href: "//soundcloud.com/g01iathonline", innerHTML: `<img src="//${window.location.hostname}/img/sc.png" alt="SoundCloud">`}),
-        socialItem2 = Object.assign(document.createElement("a"),{className: "socials-item two", target: "_blank", href: "//discord.gg/cSuzxF6ece", innerHTML: `<img src="//${window.location.hostname}/img/dc.png" alt="Discord">`}),
-        socialItem3 = Object.assign(document.createElement("a"),{className: "socials-item three", target: "_blank", href: "//g01iath.bandcamp.com", innerHTML: `<img src="//${window.location.hostname}/img/bc.png" alt="Bandcamp">`}),
-        socialItem4 = Object.assign(document.createElement("a"),{className: "socials-item four", target: "_blank", href: "//youtube.com/@g01iath", innerHTML: `<img src="//${window.location.hostname}/img/yt.png" alt="YouTube">`}),
-        children = [socialItem1, socialItem2, socialItem3, socialItem4];
+        children = [
+            Object.assign(document.createElement("a"),{className: "socials-item", target: "_blank", href: "//soundcloud.com/fritztafer", innerHTML: `<img src="//${window.location.hostname}/img/sc.png" alt="SoundCloud">`}),
+            Object.assign(document.createElement("a"),{className: "socials-item", target: "_blank", href: "//youtube.com/@fritztafer", innerHTML: `<img src="//${window.location.hostname}/img/yt.png" alt="YouTube">`}),
+            Object.assign(document.createElement("a"),{className: "socials-item", target: "_blank", href: "//fritztafer.bandcamp.com/", innerHTML: `<img src="//${window.location.hostname}/img/bc.png" alt="Bandcamp">`}),
+            Object.assign(document.createElement("a"),{className: "socials-item", target: "_blank", href: "//discord.gg/cSuzxF6ece", innerHTML: `<img src="//${window.location.hostname}/img/dc.png" alt="Discord">`}),
+            Object.assign(document.createElement("a"),{className: "socials-item", target: "_blank", href: "//github.com/fritztafer", innerHTML: `<img src="//${window.location.hostname}/img/gh.png" alt="GitHub">`})
+        ];
 
     parent.append(...children);
 
