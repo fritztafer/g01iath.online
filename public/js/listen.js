8361c9a3124f0a7837495de11dd92f515d161a33
fritztafer <fritzistheman@gmail.com>
2025-10-11 15:58:02 -0700

    modified listen embed links
    added steam icon image asset
    
    added .scroll-top:active style
    
    lowered scale on .social-item:hover style

--- public/js/listen.js
+++ public/js/listen.js
@@ -2,10 +2,9 @@ async function listen() {
     let parent = Object.assign(document.createElement("div"),{className: "listen"}),
         sources = [
             "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F2109535224&show_artwork=true&show_comments=false&secret_token=s-64sQfsfzWkM",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1354507834&show_artwork=true&show_comments=false",
             "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1289073427&show_artwork=true&show_comments=false",
             "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1272412966&show_artwork=true&show_comments=false&secret_token=s-GV2pE8tDz90",
-            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1242933133&show_artwork=true&show_comments=false"
+            "https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1242933133&show_artwork=true&show_comments=false&secret_token=s-8HKfbEp2mur"
         ];
 
     for (let src of sources) {
