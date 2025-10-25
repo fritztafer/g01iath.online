40a0d402bfacb497bfb84bfbfc66e617c964ddec
fritztafer <fritzistheman@gmail.com>
2025-10-24 20:11:59 -0700

    loop

--- get-hls.ps1
+++ get-hls.ps1
@@ -1,17 +1,21 @@
-$InFile = Read-Host -Prompt "Absolute path of file to encode"
 $EncoderDir = Read-Host -Prompt "Absolute path of ffmpeg\bin"
-$Name = Split-Path -Path $InFile -LeafBase
-$TargetDir = Join-Path ".\public\music" $Name
 
-if (Test-Path $TargetDir) {
-    Write-Warning "$TargetDir EXISTS"
-    Read-Host -Prompt "Press Enter to continue" | Out-Null
-} else {
-    Write-Host "CREATED: $TargetDir"
-    New-Item -ItemType Directory -Path $TargetDir | Out-Null
+while (1) {
+    $InFile = Read-Host -Prompt "Absolute path of file to encode"
+    $Name = Split-Path -Path $InFile -LeafBase
+    $TargetDir = Join-Path ".\public\music" $Name
+
+    if (Test-Path $TargetDir) {
+        Write-Warning "$TargetDir EXISTS"
+        Read-Host -Prompt "Press Enter to continue" | Out-Null
+    } else {
+        Write-Host "CREATED: $TargetDir"
+        New-Item -ItemType Directory -Path $TargetDir | Out-Null
+    }
+
+    & "$EncoderDir\ffmpeg.exe" -i "$InFile" -c:a aac -b:a 320k -ar 48000 -hls_time 8 -hls_playlist_type vod "$TargetDir\stream.m3u8"
+    & "$EncoderDir\ffprobe.exe" -print_format json -show_format "$TargetDir\stream.m3u8" > "$TargetDir\meta.json"
+
+    Write-Host "hls data saved to $TargetDir"
+    Read-Host -Prompt "Press Enter to encode another file"
 }
-
-& "$EncoderDir\ffmpeg.exe" -i "$InFile" -c:a aac -b:a 320k -ar 48000 -hls_time 8 -hls_playlist_type vod "$TargetDir\stream.m3u8"
-& "$EncoderDir\ffprobe.exe" -print_format json -show_format "$TargetDir\stream.m3u8" > "$TargetDir\meta.json"
-
-Write-Host "hls data saved to $TargetDir"
--- get-hls.sh
+++ get-hls.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-read -p "Absolute path of file to encode: " infile
-name=$(basename "$infile")
-name="${name%.*}"
-targetdir="./public/music/$name"
+while true; do
+    read -p "Absolute path of file to encode: " InFile
+    Name=$(basename "${InFile%.*}")
+    TargetDir="./public/music/$Name"
 
-if [ -d "$targetdir" ]; then
-    echo "EXISTS: $targetdir"
-    read -p "Press Enter to continue"
-else
-    echo "CREATED: $targetdir"
-    mkdir -p "$targetdir"
-fi
+    if [ -d "$TargetDir" ]; then
+        echo "$TargetDir EXISTS"
+        read -p "Press Enter to continue"
+    else
+        echo "CREATED: $TargetDir"
+        mkdir -p "$TargetDir"
+    fi
 
-ffmpeg -i "$infile" -c:a aac -b:a 320k -ar 48000 -hls_time 8 -hls_playlist_type vod "$targetdir/stream.m3u8"
-ffprobe -print_format json -show_format "$targetdir/stream.m3u8" > "$targetdir/meta.json"
+    ffmpeg -i "$InFile" -c:a aac -b:a 320k -ar 48000 -hls_time 8 -hls_playlist_type vod "$TargetDir/stream.m3u8"
+    ffprobe -print_format json -show_format "$TargetDir/stream.m3u8" > "$TargetDir/meta.json"
 
-echo "hls data saved to $targetdir"
+    echo "hls data saved to $TargetDir"
+    read -p "Press Enter to encode another file"
+done
