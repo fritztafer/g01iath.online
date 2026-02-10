9ee739cb65d4bc18d494af66f755f80bff7d4371
fritztafer <fritzistheman@gmail.com>
2026-02-09 16:03:53 -0800

    refactor run(), remove switchScene()
    change initialization behaviour
    
    change content transition styling
    
    change .scroll-top transition styling

--- public/main.js
+++ public/main.js
@@ -19,6 +19,7 @@ document.body.append(
 );
 
 const time = 2501;
+let initialize = true;
 let transitioning;
 
 function run(content="socials") {
@@ -27,9 +28,7 @@ function run(content="socials") {
     if (current && current.className === content) return;
     transitioning = true;
 
-    if (!current) { // initialize
-        fadeGroup(document.querySelectorAll(".header-item-parent, .header-hr, .footer-item"), "in");
-    } else { // clean up
+    if (current) {
         document.head.querySelector("script:nth-of-type(2)").remove();
         if (current.className === "aesthetic") {
             window.removeEventListener("resize", resizeHandler);
@@ -42,26 +41,29 @@ function run(content="socials") {
 
     document.head.appendChild(Object.assign(document.createElement("script"), {
         src: `//${window.location.hostname}/js/${content}.js`,
-        onload: async () => switchScene(content, await loadContent(content))
+        onload: async () => {
+            const parent = await loadContent(content);
+            document.querySelector("main").appendChild(parent);
+
+            if (initialize) {
+                transitionHandler(parent);
+                fadeGroup(document.querySelectorAll(".header-item-parent, .header-hr, .footer-item, main, #active"), "in");
+                setTimeout(() => {
+                    initialize = false;
+                    transitioning = false;
+                }, time / 2);
+            } else {
+                fadeGroup(document.querySelectorAll("main, #active"), "out");
+                setTimeout(() => {
+                    transitionHandler(parent);
+                    fadeGroup(document.querySelectorAll("main, #active"), "in");
+                }, time / 2);
+                setTimeout(() => {
+                    transitioning = false;
+                }, time);
+            }
+        }
     }));
-}
-
-function switchScene(content, parent) { // start
-    document.querySelector("main").appendChild(parent);
-    fadeGroup(document.querySelectorAll("main, #active"), "out");
-
-    setTimeout(() => { // middle
-        Object.values(document.querySelectorAll(".header-item")).forEach(item => {
-            if (item.textContent.toLowerCase() === content) item.id = "active";
-            else item.removeAttribute("id");
-        });
-        transitionHandler(parent);
-        fadeGroup(document.querySelectorAll("main, #active"), "in");
-    }, time / 2);
-
-    setTimeout(() => { // end
-        transitioning = false;
-    }, time);
 }
 
 function loadContent(content) {
@@ -78,7 +80,6 @@ function fadeGroup(elements, type) {
     elements.forEach(el => {
         const outClass = el.id === "active" ? "active-out" : "fade-out",
             inClass = el.id === "active" ? "active-in" : "fade-in";
-
         if (type === "out") {
             el.classList.remove(inClass);
             el.classList.add(outClass);
@@ -91,12 +92,13 @@ function fadeGroup(elements, type) {
 
 function transitionHandler(parent) {
     const current = document.querySelector("main").firstElementChild;
+    if (parent.className !== current.className) current.remove();
+    parent.classList.remove("hidden");
 
-    switch (parent.className) {
-        default:
-            if (parent.className !== current.className) current.remove();
-            parent.removeAttribute("style");
-    }
+    Object.values(document.querySelectorAll(".header-item")).forEach(item => {
+        if (item.textContent.toLowerCase() === parent.className) item.id = "active";
+        else item.removeAttribute("id");
+    });
 }
 
 function player(track) {
@@ -112,7 +114,8 @@ window.onscroll = () => {
     const scrollTop = document.querySelector(".scroll-top");
     if (!scrollTop && document.documentElement.scrollTop >= 100) {
         const button = Object.assign(document.createElement("button"), {
-            className: "scroll-top hidden",
+            className: "scroll-top",
+            style: "opacity: 0; visibility: hidden;",
             onclick: () => window.scrollTo({top: 0, behavior: "smooth"}),
             innerHTML: [
                 '<svg xmlns="http://www.w3.org/2000/svg" viewBox="6 0 12 24">',
@@ -121,9 +124,9 @@ window.onscroll = () => {
             ].join('')
         });
         document.body.appendChild(button);
-        setTimeout(() => button.classList.remove("hidden"), 0);
+        setTimeout(() => button.removeAttribute("style"), 0);
     } else if (scrollTop && document.documentElement.scrollTop < 100) {
-        scrollTop.classList.add("hidden");
+        scrollTop.style = "opacity: 0; visibility: hidden;";
         setTimeout(() => scrollTop.remove(), time / 4);
     }
 }
